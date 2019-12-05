b817de000b4bc4904f897b50b80b4d1f43993a0c
Haotian <htsli@ucdavis.edu>
2019-12-05 12:27:04 -0800

    update best basis algorithm

--- develop/best_basis.jl
+++ develop/best_basis.jl
@@ -1,54 +1,8 @@
 
 function best_basis_algorithm2(ht_vlist,Parent,ht_coeff_L1)
-Lvl = length(ht_vlist)
-dvec = [[Lvl+1, k] for k = 1:length(ht_vlist[Lvl])]
-Subspaces = [[tmp for tmp in pair] for pair in Parent[Lvl]]
-
-
-dvec_copy = copy(dvec)
-Subspaces_copy = copy(Subspaces)
-tmp_count = 0
-tmp_loc = []
-for i in 1:length(Subspaces)
-    global tmp_count
-    pair = Subspaces[i]
-    if length(pair) == 1
-        ind = findall(x -> x == dvec[pair[1]],dvec_copy)[1]
-        deleteat!(dvec_copy,ind)
-        insert!(dvec_copy,ind, [Lvl, i])
-        Subspaces_copy[i] = [ind]
-    else
-        if compute_subspace_cost(dvec,pair,ht_coeff_L1) > ht_coeff_L1[Lvl][i]
-            ind = findall(x -> x == dvec[pair[1]],dvec_copy)[1]
-            deleteat!(dvec_copy,ind:ind+length(pair)-1)
-            insert!(dvec_copy,ind,[Lvl, i])
-            tmp_count -= length(pair) - 1
-            push!(tmp_loc, i)
-            Subspaces_copy[i] = [ind]
-        else
-            Subspaces_copy[i] = [k + tmp_count for k in Subspaces[i]]
-        end
-    end
-end
-
-
-dvec = dvec_copy
-Subspaces = [union_array_of_arrays(Subspaces_copy[pair]) for pair in Parent[Lvl-1]]
-
-
-
-
-
-
-
-
-
-
-for lvl = Lvl-1:-1:2
-    dvec = [[lvl+1, k] for k = 1:length(ht_vlist[lvl])]
-    Subspaces = [[tmp for tmp in pair] for pair in Parent[lvl]]
-
-
+    Lvl = length(ht_vlist)
+    dvec = [[Lvl+1, k] for k = 1:length(ht_vlist[Lvl])]
+    Subspaces = [[tmp for tmp in pair] for pair in Parent[Lvl]]
     dvec_copy = copy(dvec)
     Subspaces_copy = copy(Subspaces)
     tmp_count = 0
@@ -60,38 +14,63 @@ for lvl = Lvl-1:-1:2
             ind = findall(x -> x == dvec[pair[1]],dvec_copy)[1]
             deleteat!(dvec_copy,ind)
             insert!(dvec_copy,ind, [Lvl, i])
-
             Subspaces_copy[i] = [ind]
-
         else
             if compute_subspace_cost(dvec,pair,ht_coeff_L1) > ht_coeff_L1[Lvl][i]
-
                 ind = findall(x -> x == dvec[pair[1]],dvec_copy)[1]
                 deleteat!(dvec_copy,ind:ind+length(pair)-1)
                 insert!(dvec_copy,ind,[Lvl, i])
-
                 tmp_count -= length(pair) - 1
                 push!(tmp_loc, i)
-
                 Subspaces_copy[i] = [ind]
-
             else
                 Subspaces_copy[i] = [k + tmp_count for k in Subspaces[i]]
             end
         end
     end
-
-
     dvec = dvec_copy
     Subspaces = [union_array_of_arrays(Subspaces_copy[pair]) for pair in Parent[Lvl-1]]
-end
 
+    for lvl = Lvl-1:-1:1
+        global dvec, Subspaces
+        dvec_copy = copy(dvec)
+        Subspaces_copy = copy(Subspaces)
+        tmp_count = 0
+        tmp_loc = []
+        for i in 1:length(Subspaces)
+            pair = Subspaces[i]
+            if length(pair) == 1
+                ind = findall(x -> x == dvec[pair[1]],dvec_copy)[1]
+                deleteat!(dvec_copy,ind)
+                insert!(dvec_copy,ind, [lvl, i])
+                Subspaces_copy[i] = [ind]
+            else
+                if compute_subspace_cost(dvec,pair,ht_coeff_L1) > ht_coeff_L1[lvl][i]
 
+                    ind = findall(x -> x == dvec[pair[1]],dvec_copy)[1]
+                    deleteat!(dvec_copy,ind:ind+length(pair)-1)
+                    insert!(dvec_copy,ind,[lvl, i])
 
+                    tmp_count -= length(pair) - 1
+                    push!(tmp_loc, i)
 
+                    Subspaces_copy[i] = [ind]
 
+                else
+                    Subspaces_copy[i] = [k + tmp_count for k in Subspaces[i]]
+                end
+            end
+        end
 
-return dvec
+        dvec = dvec_copy
+        if lvl > 1
+            Subspaces = [union_array_of_arrays(Subspaces_copy[pair]) for pair in Parent[lvl-1]]
+        else
+            Subspaces = [[1]]
+        end
+    end
+
+    return dvec
 end
 
 
