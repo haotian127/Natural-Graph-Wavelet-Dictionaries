99ba6e1310c261cc715aac4e749f00cc55291e27
Haotian <htsli@ucdavis.edu>
2020-03-28 20:06:29 -0700

    create systhetic traffic signal (pedestrian volume simulation)

---
+++ develop/toronto_systhetic_signal.jl
@@ -0,0 +1,56 @@
+## load packages
+using Plots, LightGraphs, JLD, LaTeXStrings, Distances, StatsBase
+include(joinpath("..", "src", "func_includer.jl"))
+
+## load graph info
+G = loadgraph(joinpath(@__DIR__, "..", "datasets", "new_toronto_graph.lgz"))
+N = nv(G)
+X = load(joinpath(@__DIR__, "..", "datasets", "new_toronto.jld"),"xy")
+W = 1.0 .* adjacency_matrix(G)
+dist_X = pairwise(Euclidean(),X; dims = 1)
+Weight = W .* dualGraph(dist_X; method = "inverse") # weighted adjacence matrix
+L = Matrix(Diagonal(sum(Weight;dims = 1)[:]) - Weight)
+
+## get a sampling distribution by considering density of vertices in terms of locations
+Q = incidence_matrix(G; oriented = true)
+edge_lengths = sqrt.(sum((Q' * X).^2, dims = 2)[:])
+thres = maximum(edge_lengths)
+vertex_distr = zeros(N)
+for i in 1:N
+    vertex_distr[i] = length(findall(dist_X[i,:] .< thres))
+end
+vertex_distr ./= sum(vertex_distr)
+N_sample = 100000
+vertex_density = zeros(N)
+for x in sample(1:N, Weights(vertex_distr), N_sample)
+    vertex_density[x] += 1
+end
+
+scatter_gplot(X;marker = vertex_density)
+
+## function to random cross N_step traffic lights
+function one_time_cross(vert_begin, G, f_signal; N_step = 5)
+    vert = vert_begin
+    for i in 1:N_step
+        f_signal[vert] += 1
+        vert = sample(neighbors(G, vert))
+    end
+    return f_signal
+end
+
+scatter_gplot(X;marker = one_time_cross(1000, G, zeros(N); N_step = 5))
+
+## create systhetic traffic signal (pedestrian volume simulation)
+
+function toronto_systhetic_signal(f, N_sample, N_step)
+    for x in sample(1:N, Weights(vertex_distr), N_sample)
+        f = one_time_cross(x, G, f; N_step = N_step)
+    end
+    return f
+end
+scatter_gplot(X;marker = toronto_systhetic_signal(zeros(N), 100000, 6))
+
+## test
+
+fp = load(joinpath(@__DIR__, "..", "datasets", "new_toronto.jld"),"fp")
+scatter_gplot(X;marker = fp)
