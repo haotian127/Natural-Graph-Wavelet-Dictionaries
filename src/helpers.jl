69c44ecaa538b6c49912ea77906cb13394b805ed
Haotian <htsli@ucdavis.edu>
2020-05-10 23:29:25 -0700

    add documentations

--- src/helpers.jl
+++ src/helpers.jl
@@ -55,28 +55,80 @@ function characteristic(list,n)
     return v
 end
 
-function heat_sol(f0,V,D,t)
-    u = V * (exp.(-t .* D) .* V' * f0)
+
+"""
+    heat_sol(f0,Φ,Σ,t)
+
+HEAT_SOL gives the solution of heat partial differential equation with initial condition u(⋅, 0) = f0
+
+# Input Arguments
+- `f0::Array{Float}`: initial condition vector.
+- `Φ::Matrix{Float}`: graph Laplacian eigenvectors, served as graph Fourier transform matrix
+- `Σ::Array{Int}`: diagonal matrix of eigenvalues.
+- `t::Float`: time elapse.
+
+# Output Argument
+- `u::Array{Float}`: the solution vector at time t
+
+"""
+function heat_sol(f0,Φ,Σ,t)
+    u = Φ * (exp.(-t .* Σ) .* Φ' * f0)
     return u
 end
 
 
+"""
+    freq_band_matrix(ls,n)
 
-function freq_band_matrix(p,n)
-    F = zeros(n,n)
-    for k = 1:length(p)
-        F[p[k],p[k]] = 1
-    end
-    return F
+FREQ_BAND_MATRIX provides characteristic diagonal matrix, which is useful for spectral graph filters design.
+
+# Input Arguments
+- `ls::Array{Int}`: list of indices.
+- `n::Int`: dimension of the target vector.
+
+# Output Argument
+- `D::Array{Float}`: the zero/one diagonal matrix.
+
+"""
+# using LinearAlgebra
+function freq_band_matrix(ls, n)
+    f = characteristic(list,n)
+    return Diagonal(f)
 end
 
 
+"""
+    scatter_gplot(X; marker = nothing, ms = 4)
+
+SCATTER_GPLOT generates a scatter plot figure, which is for quick viewing of a graph signal.
+SCATTER_GPLOT!(X; ...) adds a plot to `current` one.
+
+# Input Arguments
+- `X::Matrix{Float}`: points locations, can be 2-dim or 3-dim.
+- `marker::Array{Float}`: default is nothing. Present different colors given different signal value at each node.
+- `ms::Array{Float}`: default is 4. Present different node sizes given different signal value at each node.
+
+"""
 function scatter_gplot(X; marker = nothing, ms = 4)
-    scatter(X[:,1],X[:,2],marker_z = marker,ms = ms, c = :viridis, legend = false, mswidth = 0, cbar = true, aspect_ratio = 1)
+    dim = size(X,2)
+    if dim == 2
+        scatter(X[:,1],X[:,2],marker_z = marker,ms = ms, c = :viridis, legend = false, mswidth = 0, cbar = true, aspect_ratio = 1)
+    elseif dim == 3
+        scatter(X[:,1],X[:,2],X[:,3], marker_z = marker, ms = ms, c = :viridis, legend = false, mswidth = 0, cbar = true, aspect_ratio = 1)
+    else
+        print("Dimension Error: scatter_gplot only supports for 2-dim or 3-dim scatter plots.")
+    end
 end
 
 function scatter_gplot!(X; marker = nothing, ms = 4)
-    scatter!(X[:,1],X[:,2],marker_z = marker,ms = ms, c = :viridis, legend = false, mswidth = 0, cbar = true, aspect_ratio = 1)
+    dim = size(X,2)
+    if dim == 2
+        scatter!(X[:,1],X[:,2],marker_z = marker,ms = ms, c = :viridis, legend = false, mswidth = 0, cbar = true, aspect_ratio = 1)
+    elseif dim == 3
+        scatter!(X[:,1],X[:,2],X[:,3], marker_z = marker, ms = ms, c = :viridis, legend = false, mswidth = 0, cbar = true, aspect_ratio = 1)
+    else
+        print("Dimension Error: scatter_gplot! only supports for 2-dim or 3-dim scatter plots.")
+    end
 end
 
 function cat_plot(X; marker = nothing, ms = 4)
