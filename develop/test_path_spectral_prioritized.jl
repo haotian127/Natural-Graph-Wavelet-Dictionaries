e74d526bc26d8142cac7e20b003638acc2544ff3
Haotian <htsli@ucdavis.edu>
2020-05-11 13:16:47 -0700

    make code more compact

--- develop/test_path_spectral_prioritized.jl
+++ develop/test_path_spectral_prioritized.jl
@@ -13,28 +13,22 @@ dist_DAG = eigDAG_Distance(V,Q,N)
 # W_dual = sparse(dualGraph(dist_DAG)) #sparse dual weighted adjacence matrix
 W_dual = 1.0 * adjacency_matrix(path_graph(N))
 
+## Assemble wavelet packets
 ht_vlist, ht_elist = HTree_VElist(V,W)
 ht_elist_dual, ht_vlist_dual = HTree_EVlist(V,W_dual)
 ht_elist_varimax = ht_elist_dual
 
-
-# ht_elist_varimax = ht_elist_dual[1:4]
-# deleteat!(ht_vlist,5:7); deleteat!(ht_elist,5:7)
-# deleteat!(ht_vlist_dual,5:7); deleteat!(ht_elist_dual,5:7)
-
-
 parent_vertex = HTree_findParent(ht_vlist)
 wavelet_packet = HTree_wavelet_packet(V,ht_vlist,ht_elist)
+
+parent_varimax = HTree_findParent(ht_elist_varimax)
 wavelet_packet_varimax = HTree_wavelet_packet_varimax(V,ht_elist_varimax)
 
 parent_dual = HTree_findParent(ht_vlist_dual)
 wavelet_packet_dual = HTree_wavelet_packet(V,ht_vlist_dual,ht_elist_dual)
 
 
-wavelet_packet_dual_unortho = HTree_wavelet_packet_unorthogonalized(V,ht_vlist_dual,ht_elist_dual)
-
-
-# plot(wavelet_packet[5][1][:,10],legend = false)
+## Graph signal
 # f = [exp(-(k-N/3)^2/10)+0.5*exp(-(k-2*N/3)^2/30) for k = 1:N] .+ 0.05*randn(N); f ./= norm(f)
 f = V[:,10] + [V[1:25,20]; zeros(N-25)] + [zeros(50);V[51:end,40]]  + V[:,75]
 # f = spike(10,N)
@@ -43,6 +37,7 @@ f = V[:,10] + [V[1:25,20]; zeros(N-25)] + [zeros(50);V[51:end,40]]  + V[:,75]
 # plt = plot(f, legend = false, title = L"f = \phi_{9} + \phi_{19}(1:25) + \phi_{39}(51:end) + \phi_{74}")
 # savefig(plt, "figs/path_signal.png")
 
+## Best basis selection algorithm
 ht_coeff, ht_coeff_L1 = HTree_coeff_wavelet_packet(f,wavelet_packet)
 # C = HTree_coeff2mat(ht_coeff,N)
 dvec = best_basis_algorithm2(ht_vlist,parent_vertex,ht_coeff_L1)
@@ -50,7 +45,6 @@ Wav = assemble_wavelet_basis(dvec,wavelet_packet)
 
 ht_coeff_varimax, ht_coeff_L1_varimax = HTree_coeff_wavelet_packet(f,wavelet_packet_varimax)
 # C_varimax = HTree_coeff2mat(ht_coeff_varimax,N)
-parent_varimax = HTree_findParent(ht_elist_varimax)
 dvec_varimax = best_basis_algorithm2(ht_elist_varimax,parent_varimax,ht_coeff_L1_varimax)
 Wav_varimax = assemble_wavelet_basis(dvec_varimax,wavelet_packet_varimax)
 
@@ -76,13 +70,9 @@ Wav_dual = assemble_wavelet_basis(dvec_dual,wavelet_packet_dual)
 # plot(Wav_dual[:,idx[130]], legend = false)
 
 
-
-approx_error_plot([Wav, Wav_varimax, Wav_dual, V, I], f; label = ["WB_vertex" "WB_varimax" "WB_spectral" "Laplacian" "Standard Basis"], doSave = true, path = "figs/path_signal_approx.png")
+## plot approx. error figure w.r.t. fraction of kept coefficients
+approx_error_plot([Wav, Wav_varimax, Wav_dual, V, I], f; label = ["WB_vertex" "WB_varimax" "WB_spectral" "Laplacian" "Standard Basis"])
 current()
-
-
-
-
 
 ## generate gif files
 # heatmap(wavelet_packet_varimax[2][2])
