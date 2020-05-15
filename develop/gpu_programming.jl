e7b5d625e6f1238d70903f22d8560d3e56d6eb7d
Haotian <htsli@ucdavis.edu>
2020-05-15 16:05:40 -0700

    add some basic usage for gpu programming

--- develop/gpu_programming.jl
+++ develop/gpu_programming.jl
@@ -8,3 +8,25 @@ CuArrays.@time x_d + y_d
 
 CuArrays.memory_status()
 CuArrays.unsafe_free!
+
+##
+# A * x = b
+
+n = 1000
+
+A = sprand(Float32, n, n, 0.5)
+rank(A)
+A = sparse(A*A')
+d_A = CuArrays.CUSPARSE.CuSparseMatrixCSR(A)
+
+b = rand(Float32, n)
+d_b = CuArray(b)
+
+x = zeros(Float32, n)
+d_x = CuArray(x)
+
+tol = convert(real(Float32), 1e-6)
+d_x = CUSOLVER.csrlsvqr!(d_A, d_b, d_x, tol, one(Cint), 'O')
+h_x = collect(d_x)
+
+h_x ≈ Array(A)\b
