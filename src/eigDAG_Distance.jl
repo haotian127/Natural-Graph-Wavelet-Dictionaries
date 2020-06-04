b627db548fa75db1a26349476e359c59f8e99ec4
Haotian <htsli@ucdavis.edu>
2020-06-03 18:54:13 -0700

    shorten code by one-liner if else

--- src/eigDAG_Distance.jl
+++ src/eigDAG_Distance.jl
@@ -6,29 +6,18 @@ EIGDAG_DISTANCE compute DAG distances between pairwise graph Laplacian eigenvect
 # Input Arguments
 - `V::Matrix{Float64}`: matrix of graph Laplacian eigenvectors, 𝜙ⱼ₋₁ (j = 1,...,size(V,1)).
 - `Q::Matrix{Float64}`: incidence matrix of the graph.
-- `numEigs::Int`: number of eigenvectors considered.
-- `edge_weights`: default value is 1, stands for unweighted graph (i.e., all edge weighs equal to 1).
+- `numEigs::Int64`: number of eigenvectors considered.
+- `edge_weight::Array{Float64}`: default value is 1, stands for unweighted graph (i.e., all edge weights equal to 1).
 
 # Output Argument
 - `dis::Matrix{Float64}`: a numEigs x numEigs distance matrix, dis[i,j] = d_DAG(𝜙ᵢ₋₁, 𝜙ⱼ₋₁).
 
 """
 function eigDAG_Distance(V,Q,numEigs; edge_weight = 1)
-    # Case: Unweighted Graph
-    if edge_weight == 1
-        dis = zeros(numEigs,numEigs)
-        Ve = abs.(Q' * V)
-        for i = 1:numEigs, j = i+1:numEigs
-            dis[i,j] = norm(Ve[:,i]-Ve[:,j],2)
-        end
-    # Case: weighted Graph
-    else
-        dis = zeros(numEigs,numEigs)
-        Ve = abs.(Q' * V)
-        for i = 1:numEigs, j = i+1:numEigs
-            dis[i,j] = sqrt(sum((Ve[:,i]-Ve[:,j]).^2 .* sqrt.(edge_weight)))
-        end
+    dis = zeros(numEigs,numEigs)
+    abs_∇V = abs.(Q' * V)
+    for i = 1:numEigs, j = i+1:numEigs
+        dis[i,j] = (edge_weight == 1) ? norm(abs_∇V[:,i]-abs_∇V[:,j],2) : sqrt(sum((abs_∇V[:,i]-abs_∇V[:,j]).^2 .* sqrt.(edge_weight)))
     end
-
     return dis + dis'
 end
