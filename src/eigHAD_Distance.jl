a9971fee6e512a953105bed390d537207bc86759
Haotian <htsli@ucdavis.edu>
2020-06-03 18:10:27 -0700

    Bug fixed: diagonal entry double issue; use indexEigs instead

--- src/eigHAD_Distance.jl
+++ src/eigHAD_Distance.jl
@@ -8,13 +8,13 @@ EIGHAD\\_DISTANCE compute HAD distance between pairwise graph Laplacian eigenvec
 # Input Arguments
 - `𝛷::Matrix{Float64}`: matrix of graph Laplacian eigenvectors, 𝜙ⱼ₋₁ (j = 1,...,size(𝛷,1)).
 - `lamb::Array{Float64}`: array of eigenvalues. (ascending order)
-- `numEigs::Int`: number of eigenvectors considered.
+- `indexEigs::Int`: default is all eigenvectors, indices of eigenvectors considered.
 
 # Output Argument
 - `dis::Matrix{Float64}`: a numEigs x numEigs affinity matrix, dis[i,j] = d_HAD(𝜙ᵢ₋₁, 𝜙ⱼ₋₁).
 """
-function eigHAD_Distance(𝛷,lamb,numEigs)
-    A = eigHAD_Affinity(𝛷,lamb,numEigs)
+function eigHAD_Distance(𝛷, lamb; indexEigs = 1:size(𝛷,2))
+    A = eigHAD_Affinity(𝛷, lamb; indexEigs = indexEigs)
     dis = -log.(A)
     return dis
 end
@@ -27,32 +27,44 @@ EIGHAD_AFFINITY compute Hadamard (HAD) affinity between pairwise graph Laplacian
 # Input Arguments
 - `𝛷::Matrix{Float64}`: matrix of graph Laplacian eigenvectors, 𝜙ⱼ₋₁ (j = 1,...,size(𝛷,1)).
 - `lamb::Array{Float64}`: array of eigenvalues. (ascending order)
-- `numEigs::Int`: number of eigenvectors considered.
+- `indexEigs::Int`: default is all eigenvectors, indices of eigenvectors considered.
 
 # Output Argument
 - `A::Matrix{Float64}`: a numEigs x numEigs affinity matrix, A[i,j] = a_HAD(𝜙ᵢ₋₁, 𝜙ⱼ₋₁).
 """
-function eigHAD_Affinity(𝛷,lamb,numEigs)
-    N = size(𝛷,1)
-    A = zeros(numEigs,numEigs)
-    for i = 2:numEigs
-        tmp = Diagonal(𝛷[:,i]) * 𝛷[:,i:N]
-        measure = sqrt.(sum(tmp.^2, dims = 1))[:]
-        index = findall(measure .> .01/sqrt(N)) .+ (i - 1)
-        for j in index
-            λ, μ = lamb[i], lamb[j]
-            x₀ = 1 ./ (max(λ, μ))
-            # Find minimizer t
-            result = optimize(t -> abs(exp(-t[1]*λ) + exp(-t[1]*μ) - 1), [x₀], BFGS());
-            t = Optim.minimizer(result)[1]
-            # Compute Hadamard affinity
-            hadamardProd = 𝛷[:,i] .* 𝛷[:,j]
-            heatEvolution = 𝛷 * Diagonal(exp.(-t .* lamb)) * 𝛷' * hadamardProd
-            A[i,j] = norm(heatEvolution,2) / (norm(hadamardProd,2) + 1e-6)
+function eigHAD_Affinity(𝛷, lamb; indexEigs = 1:size(𝛷,2))
+    N, numEigs = size(𝛷,1), length(indexEigs)
+    indNoDC = setdiff(indexEigs, 1) # get rid of DC component
+    J = length(indNoDC)
+    A = zeros(J, J)
+    for a in 1:J, b in a:J
+        i, j = indNoDC[a], indNoDC[b]
+        hadamardProd = 𝛷[:,i] .* 𝛷[:,j]
+        if norm(hadamardProd,2) < 0.01/sqrt(N)
+            continue
         end
+        λ, μ = lamb[i], lamb[j]
+        x₀ = 1 ./ (max(λ, μ))
+        # Find minimizer t
+        result = optimize(t -> abs(exp(-t[1]*λ) + exp(-t[1]*μ) - 1), [x₀], BFGS());
+        t = Optim.minimizer(result)[1]
+        # Compute Hadamard affinity
+        heatEvolution = 𝛷 * Diagonal(exp.(-t .* lamb)) * 𝛷' * hadamardProd
+        A[a,b] = norm(heatEvolution,2) / (norm(hadamardProd,2) + 1e-6)
     end
-    A = A + A'
-    # Set affinity measure of 𝜙₀ with itself to be the maximum and equals to 1.
-    A[1,1] = maximum(A)
-    return A ./ A[1,1]
+    A = A + A'; for i in 1:J; A[i,i] /= 2; end
+
+    if 1 in indexEigs
+        # Set affinity measure of 𝜙₀ with itself to be the maximum and equals to 1.
+        A = matrix_augmentation(A)
+        A[1,1] = maximum(A)
+    end
+    return A ./ maximum(A)
+end
+
+function matrix_augmentation(A)
+    m, n = size(A)
+    B = zeros(m+1, n+1)
+    B[2:end, 2:end] = A
+    return B
 end
