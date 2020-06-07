5aff63e60bf4ace9e8797b43d1f5811385de9870
Haotian <htsli@ucdavis.edu>
2020-06-07 16:19:41 -0700

    add documentation for edge_weight

--- src/eigDAG_Distance.jl
+++ src/eigDAG_Distance.jl
@@ -7,7 +7,7 @@ EIGDAG_DISTANCE compute DAG distances between pairwise graph Laplacian eigenvect
 - `V::Matrix{Float64}`: matrix of graph Laplacian eigenvectors, 𝜙ⱼ₋₁ (j = 1,...,size(V,1)).
 - `Q::Matrix{Float64}`: incidence matrix of the graph.
 - `numEigs::Int64`: number of eigenvectors considered.
-- `edge_weight::Array{Float64}`: default value is 1, stands for unweighted graph (i.e., all edge weights equal to 1).
+- `edge_weight::Array{Float64}`: default value is 1, stands for unweighted graph (i.e., all edge weights equal to 1). For weighted graph, edge_weight is the weights vector, which stores the affinity weight of each edge.
 
 # Output Argument
 - `dis::Matrix{Float64}`: a numEigs x numEigs distance matrix, dis[i,j] = d_DAG(𝜙ᵢ₋₁, 𝜙ⱼ₋₁).
