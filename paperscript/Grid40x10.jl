a042ac754ce26b4eca2f15d2ece47972dd2a387c
Haotian <htsli@ucdavis.edu>
2020-08-04 23:13:14 -0700

    results after ordering the dct2d basis by eigenvalues

--- paperscript/Grid40x10.jl
+++ paperscript/Grid40x10.jl
@@ -7,6 +7,7 @@ X = zeros(N1, N2, 2); for i in 1:N1; for j in 1:N2; X[i,j,1] = i; X[i,j,2] = j; 
 L = Matrix(laplacian_matrix(G))
 𝛷 = dct2d_basis(N1, N2)
 lamb = sqrt.(sum((L*𝛷).^2, dims = 1))[:]
+ind = sortperm(lamb); lamb = lamb[ind]; 𝛷 = 𝛷[:,ind]
 Q = incidence_matrix(G; oriented = true)
 W = 1.0*adjacency_matrix(G)
 
@@ -28,7 +29,7 @@ distHAD = eigHAD_Distance(𝛷,lamb)
 
 ## Generate figure
 gr(dpi = 400) # paper standard 400 dpi
-focusEigenVecInd = [4,8,31,61,41,5]
+focusEigenVecInd = [4,5,6,9,12,14,16,17,22,27,31,58,85]
 
 for i in focusEigenVecInd
     heatmap(transpose(reshape(𝛷[:,i], N1, N2)), c = :viridis, aspect_ratio = 1); Grid_SC_plt = plot!(framestyle = :none)
@@ -40,6 +41,36 @@ for i in focusEigenVecInd
     savefig(Grid_SC_plt, "paperfigs/Grid40x10_SC_HAD_wavelet_focusEigenVec$(i-1).png")
 end
 
+
+## Non-trivial eigenvector metric
+distHAD_neglog = eigHAD_Distance_neglog(𝛷,lamb)
+
+## Build Soft Clustering NGW frame
+Ψ_HAD_neglog = SC_NGW_frame(distHAD_neglog, 𝛷; σ = 0.3, β = 4)
+
+## Generate figure
+gr(dpi = 400) # paper standard 400 dpi
+focusEigenVecInd = [4,5,6,9,12,14,16,17,22,27,31,58,85]
+
+for i in focusEigenVecInd
+    heatmap(transpose(reshape(Ψ_HAD_neglog[i,100,:], N1, N2)), c = :viridis, aspect_ratio = 1); Grid_SC_plt = plot!(framestyle = :none)
+    savefig(Grid_SC_plt, "paperfigs/Grid40x10_SC_HAD_neglog_wavelet_focusEigenVec$(i-1).png")
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ## Non-trivial eigenvector metric
 distDAG = eigDAG_Distance(𝛷, Q, N)
 
@@ -48,7 +79,7 @@ distDAG = eigDAG_Distance(𝛷, Q, N)
 
 ## Generate figure
 gr(dpi = 400) # paper standard 400 dpi
-focusEigenVecInd = [4,8,31,61,41,5]
+focusEigenVecInd = [4,5,6,9,12,14,16,17,22,27,31,58,85]
 
 for i in focusEigenVecInd
     heatmap(transpose(reshape(Ψ_DAG[i,100,:], N1, N2)), c = :viridis, aspect_ratio = 1); Grid_SC_plt = plot!(framestyle = :none)
