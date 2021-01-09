84bf27741902c39c0c807a599aa0f965817e73e9
Haotian Li <htsli@ucdavis.edu>
2021-01-09 13:12:06 -0800

    update legends order and font + HGLET results

--- paperscript/Toronto_approx_exp.jl
+++ paperscript/Toronto_approx_exp.jl
@@ -30,14 +30,14 @@ wavelet_packet_varimax = JLD.load(joinpath(@__DIR__, "..", "datasets", "Toronto_
 ## 1. f_density
 f = zeros(N); for i in 1:N; f[i] = length(findall(dist_X[:,i] .< 1/minimum(edge_weight))); end #fneighbor
 gplot(W, X; width=1); scatter_gplot!(X; marker = f, smallValFirst = true, ms = 3); signal_plt = plot!(aspect_ratio = 1, grid = false)
-savefig(signal_plt, "paperfigs/Toronto_fdensity.png")
+# savefig(signal_plt, "paperfigs/Toronto_fdensity.png")
 
 DVEC = signal_transform_coeff(f, ht_elist_dual, ht_elist_varimax, wavelet_packet_dual, wavelet_packet_varimax, 𝛷, W, X)
 
 # num_kept_coeffs = 10:10:1130; ERR = integrate_approx_results(DVEC, num_kept_coeffs, "toronto_density_DAG_k=7.csv")
 # approx_error_plot3(ERR; num_kept_coeffs = num_kept_coeffs); plot!(grid = false); approx_error_plt = current()
-approx_error_plot2(DVEC); plot!(legend = :topright); approx_error_plt = current()
-# savefig(approx_error_plt, "paperfigs/Toronto_fdensity_DAG_approx.png")
+approx_error_plot2(DVEC); plot!(legend = :topright, xguidefontsize=14, yguidefontsize=14, legendfontsize=10); approx_error_plt = current()
+savefig(approx_error_plt, "paperfigs/Toronto_fdensity_DAG_approx.png")
 
 # Show some important NGW basis vectors
 parent_dual = HTree_findParent(ht_elist_dual); Wav_dual = best_basis_selection(f, wavelet_packet_dual, parent_dual); dvec_spectral = Wav_dual' * f
@@ -56,6 +56,26 @@ for i = 2:10
     savefig(important_NGW_basis_vectors, "paperfigs/Toronto_fdensity_DAG_VM_NGW_important_basis_vector$(i).png")
 end
 
+
+# Show some important HGLET vectors
+using MTSG
+tmp=zeros(length(f),1); tmp[:,1]=f; G_Sig=GraphSig(1.0*W, xy=X, f=tmp)
+G_Sig = Adj2InvEuc(G_Sig)
+GP = partition_tree_fiedler(G_Sig,:Lrw)
+dmatrixH, dmatrixHrw, dmatrixHsym = HGLET_Analysis_All(G_Sig, GP) # expansion coefficients of 3-way HGLET bases
+dvec_hglet, BS_hglet, trans_hglet = HGLET_GHWT_BestBasis(GP, dmatrixH = dmatrixH, dmatrixHrw = dmatrixHrw, dmatrixHsym = dmatrixHsym, costfun = 1) # best-basis among all combinations of bases
+ind = sortperm(dvec_hglet.^2; rev = true)[1:10]
+# for i = 2:10
+#     important_HGLET_basis_vector, _ = HGLET_Synthesis(reshape(spike(ind[i],N), N, 1), GP, BS_hglet, G_Sig)
+#     w = important_HGLET_basis_vector[:,1]
+#     sgn = (maximum(w) > -minimum(w)) * 2 - 1
+#     gplot(W, X; width=1); scatter_gplot!(X; marker = sgn .* w, smallValFirst = true, ms = 3); important_HGLET_basis_vector_plt = plot!(grid = false)
+#     savefig(important_HGLET_basis_vector_plt, "paperfigs/Toronto_fdensity_HGLET_important_basis_vector$(i).png")
+# end
+
+for i = 2:10
+    println("(j, k, l) = ", HGLET_jkl(GP, BS_hglet.levlist[ind[i]][1], BS_hglet.levlist[ind[i]][2]))
+end
 ## 2. f_pedestrain
 fp = load(joinpath(@__DIR__, "..", "datasets", "new_toronto.jld"),"fp")
 gplot(W, X; width=1); scatter_gplot!(X; marker = fp, smallValFirst = true, ms = 3); signal_plt = plot!(aspect_ratio = 1, grid = false)
@@ -65,7 +85,7 @@ DVEC = signal_transform_coeff(fp, ht_elist_dual, ht_elist_varimax, wavelet_packe
 
 # num_kept_coeffs = 10:10:1130; ERR = integrate_approx_results(DVEC, num_kept_coeffs, "toronto_pedestrian_DAGFull_k=7.csv")
 # approx_error_plot3(ERR; num_kept_coeffs = num_kept_coeffs); plot!(grid = false, legend = :bottomleft); approx_error_plt = current()
-approx_error_plot2(DVEC); plot!(legend = :topright); approx_error_plt = current()
+approx_error_plot2(DVEC); plot!(legend = :topright, xguidefontsize=14, yguidefontsize=14, legendfontsize=10); approx_error_plt = current()
 savefig(approx_error_plt, "paperfigs/Toronto_fp_DAG_approx.png")
 
 # Show some important NGW basis vectors
