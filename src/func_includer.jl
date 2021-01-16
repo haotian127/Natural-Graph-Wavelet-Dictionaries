c65bdca651e224b32653c75f6df82d22a97a7bd9
Haotian Li <htsli@ucdavis.edu>
2021-01-16 11:31:15 -0800

    run require.jl first

--- src/func_includer.jl
+++ src/func_includer.jl
@@ -2,8 +2,9 @@
 
 filenames = readdir(joinpath(@__DIR__, "..", "src"))
 
+include(joinpath(@__DIR__, "require.jl"))
 for f in filenames
-    if f == "func_includer.jl" || f == "pSGWT.jl"
+    if f == "func_includer.jl" || f == "pSGWT.jl" || f == "require.jl"
         continue
     elseif occursin(r"^.*\.jl$",f)
         include(joinpath(@__DIR__, f))
