adf60ee4defe82aadbe998c20af3abf8a4db2c5b
Haotian Li <htsli@ucdavis.edu>
2021-01-16 11:31:47 -0800

    import partition_fiedler and gplot from MTSG.jl

--- src/require.jl
+++ src/require.jl
@@ -1,3 +1,5 @@
 # Require packages
 
-using LinearAlgebra, SparseArrays, Statistics, Arpack, Plots, LightGraphs, JLD, LaTeXStrings, Clustering, JuMP, Clp, Optim, CSV
+using LinearAlgebra, SparseArrays, Statistics, Arpack, Plots, LightGraphs, SimpleWeightedGraphs, JLD, LaTeXStrings, Clustering, JuMP, Clp, Optim, CSV, OptimalTransport, Distances, PyCall
+import StatsBase:crosscor
+import MTSG:partition_fiedler, gplot, gplot!
